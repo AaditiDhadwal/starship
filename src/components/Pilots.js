2788d11f0ee707b150de6602d7ac5fb31502d772
shashank <shashank.n@amazatic.com>
2019-12-12 16:13:37 +0530

    Component setup complete.

---
+++ src/components/Pilots.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Pilots() {
+  return <h1>This is pilots</h1>;
+}
---
+++ src/components/Starships.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Starships() {
+  return (
+    <React.Fragment>
+      <h1>This are Starships</h1>
+    </React.Fragment>
+  );
+}
